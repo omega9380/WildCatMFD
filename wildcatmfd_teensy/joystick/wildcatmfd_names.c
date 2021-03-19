cfae35ef6689e5c5b7cd00c6d12780474e7755ef
Jeremy Kellogg <omega9380@gmail.com>
2021-03-19 11:56:06 -0400

    Update wildcatmfd_names.c
    Added MANUFACTURER_NAME

--- wildcatmfd_teensy/joystick/wildcatmfd_names.c
+++ wildcatmfd_teensy/joystick/wildcatmfd_names.c
@@ -3,8 +3,17 @@
 #define PRODUCT_NAME    {'W','i','l','d','C','a','t',' ','M','F','D'}
 #define PRODUCT_NAME_LEN  11
 
+#define MANUFACTURER_NAME    {'J','e','r','e','m','y',' ','K','e','l','l','o','g','g'}
+#define MANUFACTURER_NAME_LEN  14
+
 struct usb_string_descriptor_struct usb_string_product_name = {
   2 + PRODUCT_NAME_LEN * 2,
   3,
   PRODUCT_NAME
+};
+
+struct usb_string_descriptor_struct usb_string_manufacturer_name = {
+  2 + MANUFACTURER_NAME_LEN * 2,
+  3,
+  MANUFACTURER_NAME
 };
